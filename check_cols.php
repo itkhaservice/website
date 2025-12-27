aa8c23fb0f73872e3a3f38ea498ddd37c6a2486c
itkhaservice <it@khaservice.com.vn>
2025-12-27 17:40:22 +0700

    Dọn dẹp mã nguồn: Loại bỏ các file debug, file tạm và công cụ kiểm tra database

--- check_cols.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = new mysqli('localhost', 'root', '', 'khaservice_db');
-    $result = $conn->query("SHOW COLUMNS FROM table_gioithieu");
-    while($row = $result->fetch_assoc()) {
-        echo $row['Field'] . "\n";
-    }
-?>
